3655bdc819d55da9104f0193378476fb72c32af5
Ben Phoon <phoon.ben@gmail.com>
2021-08-03 07:38:30 +0800

    Fixed generator function

--- message_generator.js
+++ message_generator.js
@@ -4,12 +4,14 @@ let food = ["Pizza", "Lasagne", "Garlic Bread", "Salad", "Bagels", "French Toast
 let drink = ["Red Wine", "Beer", "Champagne", "Cordial", "Water", "Soda", "Lemonade", "Milkshakes", "White Wine", "Coffee", "Tea", "Iced Tea", "Kombucha"]
 let location = ["the cinema", "the skatepark", "the park", "outside the shopping centre", "the fanciest restaurant in town", "the beach", "the riverside", "a friend's house"]
 
-// These select the random elements from the arrays
-let random_food = food[Math.floor(Math.random() * (food.length - 1))]
-let random_drink = drink[Math.floor(Math.random() * (drink.length - 1))]
-let random_location = location[Math.floor(Math.random() * (location.length - 1))]
+// This function selects random elements and combines into one message
+const generator = () => {
 
-// This function combines random elements into one message
-const generator = (random_food, random_drink, random_location) => {
+    let random_food = food[Math.floor(Math.random() * (food.length - 1))]
+    let random_drink = drink[Math.floor(Math.random() * (drink.length - 1))]
+    let random_location = location[Math.floor(Math.random() * (location.length - 1))]
+    
     console.log(`Let's go eat ${random_food} and drink ${random_drink} at ${random_location}!`)
 }
+
+generator()
