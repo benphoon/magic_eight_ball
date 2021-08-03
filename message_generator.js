e6dcf75123c14f98a3119531ba3663c4524ada27
Ben Phoon <phoon.ben@gmail.com>
2021-08-03 20:42:10 +0800

    Improve grammar and variety

--- message_generator.js
+++ message_generator.js
@@ -1,8 +1,8 @@
 //This message generator whill combine three seperate strings into one message.
 
-let food = ["Pizza", "Lasagne", "Garlic Bread", "Salad", "Bagels", "French Toast", "Tacos", "Hot Chips", "Fish and Chips", "Chips and Gravy", "French Onion Soup", "Toasted Ravioli", "Chilli Mussels", "Oysters", "Popcorn", "Meatball Subs"]
-let drink = ["Red Wine", "Beer", "Champagne", "Cordial", "Water", "Soda", "Lemonade", "Milkshakes", "White Wine", "Coffee", "Tea", "Iced Tea", "Kombucha"]
-let location = ["the cinema", "the skatepark", "the park", "outside the shopping centre", "the fanciest restaurant in town", "the beach", "the riverside", "a friend's house"]
+let food = ["pizza", "lasagne", "garlic bread", "salad", "bagels", "french toast", "tacos", "hot chips", "fish and chips", "chips and gravy", "french onion soup", "toasted ravioli", "chilli mussels", "oysters", "popcorn", "meatball subs", "cookies", "cupcakes"]
+let drink = ["red wine", "beer", "champagne", "cordial", "water", "soda", "lemonade", "milkshakes", "white wine", "coffee", "tea", "iced tea", "kombucha", "orange juice", "apple juice", "mojitos", "chocolate milk"]
+let location = ["the cinema", "the skatepark", "the park", "the shopping centre", "the fanciest restaurant in town", "the beach", "the riverside", "a friend's house", "the dog park", "the office", "the rooftop", "the big hill", "the lighthouse", "the beach cave"]
 
 // This function selects random elements and combines into one message
 const generator = () => {
